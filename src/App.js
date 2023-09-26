22af358e544c44fd757df0b3e238f1e1239d0ba0
supriya <supriyajha2108@gmail.com>
2023-09-26 14:01:36 +0530

    app ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,39 +1,87 @@
-import React, { useState } from 'react'
-import GeneratedStory from './GeneratedStory'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
+import { RWebShare } from "react-web-share";
 const App = () => {
-  
   const [prompt,setPrompt]=useState('')
-  const [dummy,setDummy]=useState('')
   const [story,setStory]=useState('')
   const PromptShare=(event)=>{
     event.preventDefault()
-    setPrompt(dummy)
+    const data={key1:prompt}
+    fetch("http://127.0.0.1:8080/story",{
+      method: "POST",
+      headers: {
+          'Content-Type': 'application/json', // Set the Content-Type header
+      },
+      body:JSON.stringify(data.key1)
+  }).then( response => response.text() )
+  .then( text => {setStory(text)} )
+  .catch((error) => {
+      // Handle any errors
+      console.error('Error sending data:', error);
+  });
+  
+}
+const downloadText = () => {
+  const text = story; // Get the content from the textarea
 
-    axios.post('/generate_story', { prompt })
-            .then((response) => {
-                setStory(response.data);
-            })
-            .catch((error) => {
-                console.error('Error generating story:', error);
-            });
-    };
-    
+  // Create a new Blob object with the text content
+  const blob = new Blob([text], { type: "text/plain" });
+
+  // Create a temporary <a> element and set its download attribute to specify the file name
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "file.txt"; // Replace with your desired file name
+
+  // Programmatically click the link to trigger the download
+  link.click();
+
+  // Clean up the URL.createObjectURL by revoking the object URL after some time
+  setTimeout(() => {
+    URL.revokeObjectURL(link.href);
+  }, 100);
+};
   
 
   const handlePromptChange=(event)=>{
-    setDummy(event.target.value);
+    setPrompt(event.target.value);
     
   }
 
+  // API TO  SHARE WITH
+const share=()=>{
+  if (navigator.share) {
+    navigator.share({
+        title: 'YOUR STORY',
+        text:story
+    }).then(() => {
+        console.log('Thanks for sharing!');
+    }).catch(err => {
+
+        // Handle errors, if occurred
+        console.log(
+        "Error while using Web share API:");
+        console.log(err);
+    });
+} else {
+
+    // Alerts user if API not available
+    alert("Browser doesn't support this API !");
+}
+}
   return (
     <>
-     <form>
-      <label>Enter the prompt</label>
-      <textarea name="prompt" value={dummy} onChange={handlePromptChange}/>
-      <button type='submit' onClick={PromptShare}>Submit</button>
-      <GeneratedStory prompt={prompt}/>
+    <div style={{marginLeft:"20%",marginTop:"2%"}}>
+    <form>
+      <label>Enter the prompt for the story you want to create</label><br/>
+      <input type="text" name="prompt" value={prompt} onChange={handlePromptChange}/>
+      <button type='submit' onClick={PromptShare}>Submit</button><br/><br/><br/>
+     
       </form> 
+      {
+        story!==""?<><label style={{fontWeight:"bold"}}>YourStory</label><br/><textarea value={story} style={{height:"400px",width:"600px",border:"none"}} /><br/><button onClick={downloadText}>Save</button><button onClick={share}>Share</button></>:""
+      }
+    </div>
+    
     </>
   )
 }
--- src/app.py
+++ src/app.py
@@ -1,21 +1,43 @@
-
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,json
 import subprocess
+from flask_cors import CORS
+import os
+import openai
+ 
+import sys
+openai.api_key =  "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"
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/generate_story', methods=['POST'])
-def generate_story():
-    try:
-        prompt = request.json.get('prompt')
-        # Call your Python script with the provided prompt
-        result = subprocess.check_output(['python', 'ai-content.py', prompt], text=True)
-        return jsonify({'story': result})
-    except Exception as e:
-        return jsonify({'error': str(e)})
+@app.route('/story',methods=['GET','POST'] ,strict_slashes=False)
+def story_gen():
+      data=request.get_json()
+      response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                  {
+                  "role": "user",
+                  "content": "Generate a story on: {} ".format(data)
+                  }
+            ],
+            temperature=1,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+            )
+      
+      
+      if 'choices' in response:
+           if (len(response['choices'])>0):
+                answer=response['choices'][0]['message']['content']
+                return(answer) 
+      
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True ,port=8080,use_reloader=False)
 
 
 
